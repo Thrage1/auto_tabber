6ebab59aa92c18fb71de88cb144e2303a7ea5daa
David Veytsman <dveyts@yahoo.com>
2018-05-30 15:43:02 -0400

    entering valid url opens new page now

--- popup.js
+++ popup.js
@@ -8,10 +8,9 @@ document.addEventListener("DOMContentLoaded",function(){
   open.addEventListener("click", function(event) {
     event.preventDefault();
 
-    var links = document.getElementsByTagName('a');
-    for(var i = 0; i < links.length; i++){
-      var link = links[i];
-      chrome.tabs.create({url: link.href, selected: false})
+    for(var i = 0; i < urls.length; i++){
+      var url = urls[i];
+      chrome.tabs.create({url: url.value, selected: false})
     }
     window.close();
 
