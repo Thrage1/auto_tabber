285e02397fb940bd98f6644a554c133b844b58e9
David Veytsman <dveyts@yahoo.com>
2018-05-30 15:39:15 -0400

    cleaned up the popup.js file

--- popup.js
+++ popup.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded",function(){
   let urls = document.getElementsByClassName('urls');
+
   console.log(urls);
+
   let open = document.getElementById('open');
+
   open.addEventListener("click", function(event) {
     event.preventDefault();
+
     var links = document.getElementsByTagName('a');
     for(var i = 0; i < links.length; i++){
       var link = links[i];
       chrome.tabs.create({url: link.href, selected: false})
     }
     window.close();
+
   });
 });
 
