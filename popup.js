4e8d84b41d8844957bb81b9f9758bde060389daf
David Veytsman <dveyts@yahoo.com>
2018-05-30 16:45:53 -0400

    added functionality to the ext and fixed bugs

--- popup.js
+++ popup.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded",function(){
-  let urls = document.getElementsByClassName('urls');
-
-  console.log(urls);
 
   let open = document.getElementById('open');
 
+  document.addEventListener('keypress', function(e){
+    if (e.keyCode === 13 && e.srcElement.type !== 'button'){
+      console.log(e.target);
+      let list = document.getElementById('list');
+      let nextField = document.createElement("input");
+      nextField.classList.add('urls');
+      list.appendChild( nextField );
+    }
+  });
+
   open.addEventListener("click", function(event) {
     event.preventDefault();
-
+    let urls = document.getElementsByClassName('urls');
+    console.log(urls);
     for(var i = 0; i < urls.length; i++){
       var url = urls[i];
       chrome.tabs.create({url: url.value, selected: false})
